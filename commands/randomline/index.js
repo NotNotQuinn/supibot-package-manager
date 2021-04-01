602efdb80ae6223e16403457920797e090a44413
Quinn T <Quinn.GitHub@gmail.com>
2021-04-01 12:49:04 -0500

    fix dank randomline fixed values

--- commands/randomline/index.js
+++ commands/randomline/index.js
@@ -32,164 +32,73 @@ module.exports = {
 					reply: "User not found in the database!"
 				};
 			}
-	
-			if (channelID === 7 || channelID === 8 || channelID === 82) {
-				const channels = ((channelID === 82) ? [27, 45, 82] : [7, 8, 46]).map(i => sb.Channel.get(i));
-				const counts = (await Promise.all(
-					channels.map(channel => sb.Query.getRecordset(rs => rs
-						.select("IFNULL(Message_Count, 0) AS Messages")
-						.from("chat_data", "Message_Meta_User_Alias")
-						.where("User_Alias = %n", targetUser.ID)
-						.where("Channel = %n", channel.ID)
-						.single()
-					))
-				)).map(i => i?.Messages ?? 0);
-	
-				const randomID = sb.Utils.random(1, counts.reduce((prev, cur) => prev += cur, 0));
-				let targetID = null;
-				let targetChannel = null;
-	
-				if (randomID < counts[0]) {
-					targetID = randomID;
-					targetChannel = channels[0];
-				}
-				else if (randomID < (counts[0] + counts[1])) {
-					targetID = randomID - counts[0];
-					targetChannel = channels[1];
-				}
-				else {
-					targetID = randomID - counts[0] - counts[1];
-					targetChannel = channels[2];
-				}
-	
-				const data = await sb.Query.getRecordset(rs => rs
-					.select("ID")
-					.from("chat_line", targetChannel.getDatabaseName())
-					.where("User_Alias = %n", targetUser.ID)
-					.limit(1)
-					.offset(targetID)
-					.single()
-				);
-	
-				if (!data) {
-					return {
-						reply: "That user did not post any lines in any of the relevant channels here!"
-					};
-				}
-	
-				result = await sb.Query.getRecordset(rs => rs
-					.select("Text", "Posted", `"${targetUser.Name}" AS Name`)
-					.from("chat_line", targetChannel.getDatabaseName())
-					.where("ID >= %n", data.ID)
-					.orderBy("ID ASC")
-					.limit(1)
-					.single()
-				);
+
+			const data = await sb.Query.getRecordset(rs => rs
+				.select("Message_Count AS Count")
+				.from("chat_data", "Message_Meta_User_Alias")
+				.where("User_Alias = %n", targetUser.ID)
+				.where("Channel = %n", channelID)
+				.single()
+			);
+
+			if (!data) {
+				return {
+					reply: "That user has not posted any messages in this channel!"
+				};
 			}
-			else {
-				const data = await sb.Query.getRecordset(rs => rs
-					.select("Message_Count AS Count")
-					.from("chat_data", "Message_Meta_User_Alias")
-					.where("User_Alias = %n", targetUser.ID)
-					.where("Channel = %n", channelID)
-					.single()
-				);
-	
-				if (!data) {
-					return {
-						reply: "That user has not posted any messages in this channel!"
-					};
-				}
-				else if (!data.Count) {
-					return {
-						reply: "That user has no metadata associated with them!"
-					};
-				}
-	
-				const random = await sb.Query.getRecordset(rs => rs
-					.select("ID")
-					.from("chat_line", channelName)
-					.where("User_Alias = %n", targetUser.ID)
-					.limit(1)
-					.offset(sb.Utils.random(1, data.Count) - 1)
-					.single()
-				);
-	
-				if (!random) {
-					return {
-						reply: "No messages could be fetched!"
-					};
-				}
-	
-				result = await sb.Query.getRecordset(rs => rs
-					.select("Text", "Posted", `"${targetUser.Name}" AS Name`)
-					.from("chat_line", channelName)
-					.where("ID >= %n", random.ID)
-					.orderBy("ID ASC")
-					.limit(1)
-					.single()
-				);
+			else if (!data.Count) {
+				return {
+					reply: "That user has no metadata associated with them!"
+				};
 			}
+
+			const random = await sb.Query.getRecordset(rs => rs
+				.select("ID")
+				.from("chat_line", channelName)
+				.where("User_Alias = %n", targetUser.ID)
+				.limit(1)
+				.offset(sb.Utils.random(1, data.Count) - 1)
+				.single()
+			);
+
+			if (!random) {
+				return {
+					reply: "No messages could be fetched!"
+				};
+			}
+
+			result = await sb.Query.getRecordset(rs => rs
+				.select("Text", "Posted", `"${targetUser.Name}" AS Name`)
+				.from("chat_line", channelName)
+				.where("ID >= %n", random.ID)
+				.orderBy("ID ASC")
+				.limit(1)
+				.single()
+			);
+
 		}
 		else {
-			if (channelID === 7 || channelID === 8 || channelID === 82) {
-				const channels = ((channelID === 82) ? [27, 45, 82] : [7, 8, 46]).map(i => sb.Channel.get(i).getDatabaseName());
-				const counts = (await Promise.all(channels.map(channel =>
-					sb.Query.getRecordset(rs => rs
-						.select("MAX(ID) AS Total")
-						.from("chat_line", channel)
-						.single()
-					)
-				))).map(i => i.Total);
-	
-				const ID = sb.Utils.random(1, counts.reduce((acc, cur) => acc += cur, 0));
-				let targetID = null;
-				let targetChannel = null;
-	
-				if (ID < counts[0]) {
-					targetID = ID;
-					targetChannel = channels[0];
-				}
-				else if (ID < (counts[0] + counts[1])) {
-					targetID = ID - counts[0];
-					targetChannel = channels[1];
-				}
-				else {
-					targetID = ID - counts[0] - counts[1];
-					targetChannel = channels[2];
-				}
-	
-				result = await sb.Query.getRecordset(rs => rs
-					.select("Text", "Posted", "Name")
-					.from("chat_line", targetChannel)
-					.join("chat_data", "User_Alias")
-					.where(targetChannel + ".ID = %n", targetID)
-					.single()
-				);
+			const data = await sb.Query.getRecordset(rs => rs
+				.select("MAX(ID) AS Total")
+				.from("chat_line", channelName)
+				.single()
+			);
+
+			if (!data || !data.Total) {
+				return {
+					reply: "This channel doesn't have enough chat lines saved yet!"
+				};
 			}
-			else {
-				const data = await sb.Query.getRecordset(rs => rs
-					.select("MAX(ID) AS Total")
-					.from("chat_line", channelName)
-					.single()
-				);
-	
-				if (!data || !data.Total) {
-					return {
-						reply: "This channel doesn't have enough chat lines saved yet!"
-					};
-				}
-	
-				result = await sb.Query.getRecordset(rs => rs
-					.select("Text", "Posted", "Name")
-					.from("chat_line", channelName)
-					.join("chat_data", "User_Alias")
-					.where("`" + channelName + "`.ID >= %n", sb.Utils.random(1, data.Total))
-					.orderBy("`" + channelName + "`.ID ASC")
-					.limit(1)
-					.single()
-				);
-			}
+
+			result = await sb.Query.getRecordset(rs => rs
+				.select("Text", "Posted", "Name")
+				.from("chat_line", channelName)
+				.join("chat_data", "User_Alias")
+				.where("`" + channelName + "`.ID >= %n", sb.Utils.random(1, data.Total))
+				.orderBy("`" + channelName + "`.ID ASC")
+				.limit(1)
+				.single()
+			);
 		}
 	
 		const partialReplies = [{
