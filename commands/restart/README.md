2de6c7e859c376bd4ff3e74e011b95b41d0b929f
Quinn T <Quinn.GitHub@gmail.com>
2021-04-03 11:42:46 -0500

    dank restart command updates

---
+++ commands/restart/README.md
@@ -0,0 +1,6 @@
+This command uses the following config variables:
+
+- `DEFAULT_ORIGIN_GIT_BRANCH_NAME` **NOT** setup by the setup script.
+  - Branch to pull from when pulling from origin. ex. `git pull origin ${branch}`
+- `DEFAULT_UPSTREAM_GIT_BRANCH_NAME` **NOT** setup by the setup script.
+  - Branch to pull from when pulling from upstream. ex. `git pull upstream ${branch}`
--- commands/restart/index.js
+++ commands/restart/index.js
@@ -3,18 +3,27 @@ module.exports = {
 	Aliases: null,
 	Author: "supinic",
 	Cooldown: 0,
-	Description: "Restarts the bot/website process via pm2, optionally also git-pulls changes and/or upgrades the supi-core module.",
+	Description: "Restarts the bot/website process, optionally also git-pulls changes and/or upgrades the supi-core module.",
 	Flags: ["read-only","system","whitelist"],
-	Params: null,
+	Params: [
+		{
+			name: "branch",
+			type: "string"
+		},
+		{
+			name: "upstream",
+			type: "boolean"
+		}
+	],
 	Whitelist_Response: "Only available to administrators!",
 	Static_Data: (() => ({
 		dir: {
-			bot: "/code/supibot",
-			web: "/code/web"
+			bot: "d:\\dev\\node\\not-my-stuff\\supibot",
+			web: ""
 		},
-		pm2: {
-			bot: "pm2 restart supibot",
-			web: "sudo pm2 restart web"
+		start: {
+			bot: "npm run debug --prefix d:\\dev\\node\\not-my-stuff\\supibot",
+			web: "echo \"Fatal error: Web does not exist!!!\" && exit 1"
 		}
 	})),
 	Code: (async function restart (context, ...types) {
@@ -28,17 +37,19 @@ module.exports = {
 	
 		const queue = [];
 		const dir = this.staticData.dir[processType];
-		const pm2 = this.staticData.pm2[processType];
+		const start = this.staticData.start[processType];
 		const respond = (context.channel)
 			? (string) => context.channel.send(string)
 			: (string) => context.platform.pm(string, context.user.Name);
 	
 		if (types.includes("all") || types.includes("pull")) {
 			queue.push(async () => {
-				await respond("VisLaud 👉 git pull origin master");
+				let source = context.params.upstream ? "upstream" : "origin"
+				let branch = context.params.branch ?? (context.params.upstream ? sb.Config.get("DEFAULT_UPSTREAM_GIT_BRANCH_NAME") : sb.Config.get("DEFAULT_ORIGIN_GIT_BRANCH_NAME"))
+				await respond(`VisLaud 👉 git pull ${source} ${branch}`);
 	
 				await shell(`git -C ${dir} checkout -- yarn.lock package.json`);
-				const result = await shell(`git -C ${dir} pull origin master`);
+				const result = await shell(`git -C ${dir} pull ${source} ${branch}`);
 				console.log("pull result", { stdout: result.stdout, stderr: result.stderr });
 			});
 		}
@@ -53,7 +64,7 @@ module.exports = {
 	
 		queue.push(async () => {
 			await respond("VisLaud 👉 Restarting process");
-			setTimeout(() => shell(pm2), 1000);
+			setTimeout(() => shell(start), 1000);
 		});
 	
 		for (const fn of queue) {
