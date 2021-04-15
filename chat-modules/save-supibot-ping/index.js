f3fcef4e0b17f1f35b44341b34eea71f5e719c1d
Quinn T <Quinn.GitHub@gmail.com>
2021-04-15 00:20:23 -0500

    Fix save-supibot-ping chat module

--- chat-modules/save-supibot-ping/index.js
+++ chat-modules/save-supibot-ping/index.js
@@ -1,21 +1,17 @@
 module.exports = {
 	Name: "save-supibot-ping",
 	Events: ["message"],
-	Description: "This module saves supibot's latency to TMI, so it can be used to compare in wanductbots own ping command.",
-	Code: (async function pingQuinn (context) {
-		const { message, user, platform } = context;
+	Description: "This module saves supibot's latency to chat, so it can be used to compare in wanductbots own ping command.",
+	Code: (async function saveSupibotPing (context) {
+		const { message, user } = context;
 
-        if (user.ID !== sb.User.get("supibot").ID) return;
+        if (user.ID !== await sb.User.get("supibot").ID) return;
 
         const regex = /^P.ng!.*Latency to TMI: (\d+)ms/
-
-        let ping = regex.exec(message)[1];
-
-        if (typeof ping !== "string") return;
-
-        ping = Number(ping)
-
-        this.data.supibot_latency = ping
+        if (regex.test(message)) {
+            let match = regex.exec(message);
+            this.data.supibot_latency = match[1]
+        }
 	}),
-	Author: "quinn"
+	Author: "quinndt"
 };
