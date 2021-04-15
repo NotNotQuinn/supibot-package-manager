6f41d5a6008d53cfc9c83c036ed24978431f11e2
Quinn T <Quinn.GitHub@gmail.com>
2021-04-15 03:47:31 -0500

    Update save-supibot-ping to save-supibot-ping-real-this-time-for-sure

--- chat-modules/save-supibot-ping/index.js
+++ chat-modules/save-supibot-ping/index.js
@@ -3,15 +3,16 @@ module.exports = {
 	Events: ["message"],
 	Description: "This module saves supibot's latency to chat, so it can be used to compare in wanductbots own ping command.",
 	Code: (async function saveSupibotPing (context) {
-		const { message, user } = context;
-
-        if (user.ID !== await sb.User.get("supibot").ID) return;
+        const { message, user } = context;
+    
+        let supibot = await sb.User.get("supibot");
+        if (user.ID != supibot.ID) return;
 
         const regex = /^P.ng!.*Latency to TMI: (\d+)ms/
         if (regex.test(message)) {
             let match = regex.exec(message);
-            this.data.supibot_latency = match[1]
+            this.data.supibot_latency = match[1];
         }
-	}),
+    }),
 	Author: "quinndt"
 };
